f9375857da877212655a031a9ad46bdec8db991f
Mi460 <MCMi460@users.noreply.github.com>
2020-06-09 18:01:25 -0500

    Update accessory: CreeperWand
    Changed directories and successfully became an accessory. Not fully tested yet.

--- Source/EyeMod/Items/Accessories/CreeperWand.cs
+++ Source/EyeMod/Items/Accessories/CreeperWand.cs
@@ -3,26 +3,26 @@ using Terraria.ModLoader;
 
 namespace EyeMod.Items
 {
-	public class CreeperRod : ModItem
+	public class CreeperWand : ModItem
 	{
 		public override void SetStaticDefaults() 
 		{
-			// DisplayName.SetDefault("CreeperRod"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-			Tooltip.SetDefault("This is a placeholder sword for a useable item in the future.");
+			// DisplayName.SetDefault("CreeperWand"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("This is a placeholder accessory for a useable item in the future.");
 		}
 
 		public override void SetDefaults() 
 		{
 			item.damage = 1;
 			item.melee = true;
-			item.width = 40;
-			item.height = 40;
-			item.useTime = 20;
+			item.width = 60;
+			item.height = 80;
+			item.useTime = 10;
 			item.useAnimation = 20;
 			item.useStyle = 1;
 			item.knockBack = 6;
 			item.value = 10000;
-			item.rare = 2;
+			item.rare = -12;
 			item.UseSound = SoundID.Item1;
 			item.autoReuse = true;
 		}
