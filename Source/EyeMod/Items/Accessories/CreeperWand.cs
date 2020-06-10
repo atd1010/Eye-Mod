66ac907786d8b1557fb6e9db2c9a6f80f7e4d769
Mi460 <MCMi460@users.noreply.github.com>
2020-06-09 20:14:42 -0500

    Various additions and updates
    Edited creeper wand, still not a final item.
    Added King Creeper summon item. Not final either, but not really going to change much in the future.
    
    -MCMi460

--- Source/EyeMod/Items/Accessories/CreeperWand.cs
+++ Source/EyeMod/Items/Accessories/CreeperWand.cs
@@ -7,12 +7,10 @@ namespace EyeMod.Items.Accessories
 {
 	public class CreeperWand : ModItem
 	{
-		public override void SetStaticDefaults() 
+		public override void SetStaticDefaults()
 		{
-			// DisplayName.SetDefault("CreeperWand"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
 			Tooltip.SetDefault("Double tap a direction to teleport a far distance.");
 		}
-
 		public override void SetDefaults() 
 		{
 			item.width = 20;
@@ -20,15 +18,6 @@ namespace EyeMod.Items.Accessories
 			item.accessory = true;
 			item.value = 450000;
 			item.rare = -12;
-		}
-
-		public override void AddRecipes() 
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DirtBlock, 10);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
 		}
 	}
 }
